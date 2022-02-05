95a93bc41f2bcda7c801187c626bdf0c97f169ea
Mydpersonalproject <elizabeth.munanye@mpesafoundationacademy.ac.ke>
2022-02-05 18:06:32 +0300

    registered sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
            timeFormat: 'H:mm',
            interval: 5,
        });
+  async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('../sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
+
        
      // Initialize Firebase
        var config = {
